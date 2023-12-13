2859b4d45a21c6917dbb6c939bcb9f2001e12d6c
alex <alex@dytry.ch>
2023-12-13 17:35:51 +0000

    main: class completions assistant for sandbox

---
+++ src/demos/competions-commands/CompletionsCommandsAssistant.ts
@@ -0,0 +1,137 @@
+import { Editor } from '@tldraw/tldraw'
+import OpenAI from 'openai'
+import { ChatCompletionStream } from 'openai/lib/ChatCompletionStream.mjs'
+import { Assistant, Thread } from '../../Assistant'
+import { fetchText } from '../../lib/fetchText'
+import { assert } from '../../lib/utils'
+import { EditorDriverApi } from './EditorDriverApi'
+import commandsPrompt from './completions-prompt.md'
+import { getUserMessage } from './getUserMessage'
+
+const apiKey = process.env.OPENAI_API_KEY ?? null
+const assistantId = process.env.OPENAI_ASSISTANT_ID ?? null
+
+if (!apiKey) {
+	throw Error(
+		`Error: OpenAI API key not found, please create an API Key in the OpenAI platform and add it as .env.VITE_OPENAI_API_KEY`
+	)
+}
+
+if (!assistantId) {
+	throw Error(
+		`Error: Assistant ID not found, please create an assistant in the OpenAI platform playground and add its id to .env.VITE_OPENAI_ASSISTANT_ID`
+	)
+}
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY!,
+	dangerouslyAllowBrowser: true,
+})
+
+export class CompletionCommandsAssistant implements Assistant<ChatCompletionStream> {
+	constructor() {}
+
+	systemPromptPromise: Promise<string> | null = null
+	getSystemPrompt() {
+		if (!this.systemPromptPromise) {
+			this.systemPromptPromise = fetchText(commandsPrompt)
+		}
+		return this.systemPromptPromise
+	}
+
+	async createThread(editor: Editor) {
+		const systemPrompt = await this.getSystemPrompt()
+		return new CompletionCommandsThread(systemPrompt, editor)
+	}
+}
+
+export class CompletionCommandsThread implements Thread<ChatCompletionStream> {
+	messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[]
+
+	constructor(
+		systemPrompt: string,
+		readonly editor: Editor
+	) {
+		this.messages = [
+			{
+				role: 'system',
+				content: systemPrompt,
+			},
+		]
+	}
+
+	getUserMessage(input: string) {
+		return getUserMessage(this.editor, input)
+	}
+
+	currentStream: ChatCompletionStream | null = null
+
+	async sendMessage(userMessage: string) {
+		if (this.currentStream) {
+			throw Error(`Error: already sending message`)
+		}
+
+		this.messages.push({
+			role: 'user',
+			content: userMessage,
+		})
+
+		const stream = openai.beta.chat.completions.stream({
+			model: 'gpt-4',
+			messages: this.messages,
+		})
+
+		this.currentStream = stream
+
+		return stream
+	}
+
+	async cancel() {
+		if (this.currentStream) {
+			this.currentStream.abort()
+			this.currentStream = null
+		}
+	}
+
+	async handleAssistantResponse(stream: ChatCompletionStream) {
+		assert(this.currentStream === stream)
+
+		const api = new EditorDriverApi(this.editor)
+
+		return new Promise<void>((resolve, reject) => {
+			stream.on('content', (_delta, snapshot) => {
+				if (stream.aborted) return
+
+				// Tell the driver API to process the whole snapshot
+				api.processSnapshot(snapshot)
+			})
+
+			stream.on('finalContent', (snapshot) => {
+				if (stream.aborted) return
+
+				// Tell the driver API to complete
+				api.complete()
+
+				// Add the assistant's response to the thread
+				this.messages.push({
+					role: 'assistant',
+					content: snapshot,
+				})
+				resolve()
+			})
+
+			stream.on('abort', () => {
+				resolve()
+			})
+
+			stream.on('error', (err) => {
+				console.error(err)
+				resolve()
+			})
+
+			stream.on('end', () => {
+				resolve()
+			})
+		})
+	}
+}
--- src/demos/competions-commands/CompletionsDemo.tsx
+++ src/demos/competions-commands/CompletionsDemo.tsx
@@ -1,24 +1,61 @@
 import { TLEditorComponents, Tldraw, useEditor } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { UserPrompt } from '../../components/UserPrompt'
-import { getUserMessage } from './getUserMessage'
-import { useCompletions } from './useCompletions'
+import { assert } from '../../lib/utils'
+import {
+	CompletionCommandsAssistant,
+	CompletionCommandsThread,
+} from './CompletionsCommandsAssistant'
 
 const components: TLEditorComponents = {
 	InFrontOfTheCanvas: () => {
 		const editor = useEditor()
-		const { start, restart, cancel } = useCompletions()
+		const assistant = useMemo(() => new CompletionCommandsAssistant(), [])
+		const [thread, setThread] = useState<CompletionCommandsThread | null>(null)
 
-		const startWithPrompt = useCallback(
+		useEffect(() => {
+			let isCancelled = false
+			;(async () => {
+				const thread = await assistant.createThread(editor)
+				if (isCancelled) return
+				setThread(thread)
+			})()
+			return () => {
+				isCancelled = true
+			}
+		}, [assistant, editor])
+
+		useEffect(() => {
+			if (!thread) return
+			return () => {
+				thread.cancel()
+			}
+		}, [thread])
+
+		const start = useCallback(
 			async (input: string) => {
-				const userMessage = getUserMessage(editor, input)
-				await start(userMessage)
+				assert(thread)
+				const userMessage = thread.getUserMessage(input)
+				const result = await thread.sendMessage(userMessage)
+				await thread.handleAssistantResponse(result)
 			},
-			[editor, start]
+			[thread]
 		)
 
-		return <UserPrompt assistant={{ start: startWithPrompt, restart, cancel }} />
+		const restart = useCallback(async () => {
+			const newThread = await assistant.createThread(editor)
+			setThread(newThread)
+		}, [assistant, editor])
+
+		const cancel = useCallback(async () => {
+			assert(thread)
+			await thread.cancel()
+		}, [thread])
+
+		if (!thread) return null
+
+		return <UserPrompt assistant={{ start, restart, cancel }} />
 	},
 }
 
